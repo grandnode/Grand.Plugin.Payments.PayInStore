0b069deb34e451b815a8cdc10f6d2c627ad2faa2
PatrykPorabik <patryk@porabik.pl>
2020-09-16 13:35:24 +0200

    init commint

---
+++ Models/PaymentInfoModel.cs
@@ -0,0 +1,9 @@
+﻿using Grand.Core.Models;
+
+namespace Grand.Plugin.Payments.PayInStore.Models
+{
+    public class PaymentInfoModel : BaseModel
+    {
+        public string DescriptionText { get; set; }
+    }
+}
